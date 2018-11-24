21bda880b1cf97350a01bd9262895a8175923282
정찬영 <jeongchan-yeong@Juicy0429.local>
2018-11-24 16:22:56 +0900

    models fix

--- models/question.js
+++ models/question.js
@@ -1,15 +1,21 @@
-var mongoose = require('mongoose'),
-    mongoosePaginate = require('mongoose-paginate'),
-    Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate');
+const Schema = mongoose.Schema;
 
 var schema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User' },
   title: {type: String, trim: true, required: true},
   content: {type: String, trim: true, required: true},
+  contest_period: {type: String, trim: true, required: true},
+  test_object: {type: String, trim: true, required: true},
+  sponser: {type: String, trim: true, required: true},
+  staff: {type: String, trim: true, required: true},
+  tel: {type: String, trim: true, required: true},
   tags: [String],
   numLikes: {type: Number, default: 0},
   numAnswers: {type: Number, default: 0},
   numReads: {type: Number, default: 0},
+  img: {type: String},
   createdAt: {type: Date, default: Date.now}
 }, {
   toJSON: { virtuals: true},
